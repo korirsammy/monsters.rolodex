b6fbc7e7e0262aaf40ee5ebaea7efcf5b944d35b
Korir <sammy.korir@bell.corp.bce.ca>
2020-01-08 14:51:02 -0400

    Updates

---
+++ precache-manifest.5a4409ecaa4e3660aaaf3c5f48d1ee68.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d4e29b7eca430e868c77eddcb1bd0a1",
+    "url": "/io/monsters.rolodex/index.html"
+  },
+  {
+    "revision": "3e6079f07470bdf2d571",
+    "url": "/io/monsters.rolodex/static/css/main.5482522f.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/io/monsters.rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/io/monsters.rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3e6079f07470bdf2d571",
+    "url": "/io/monsters.rolodex/static/js/main.7edebec6.chunk.js"
+  },
+  {
+    "revision": "292b62f9f36d3340215c",
+    "url": "/io/monsters.rolodex/static/js/runtime-main.af4bdab3.js"
+  }
+]);
